ef07b4b47749056fe4c8db0c07db71cc3cbe0662
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-23 13:59:02 -0600

    ✨ feat: update Layout component with header and main structure

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,9 +3,18 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <div className="text-8xl">Layout</div>
-
-      <Outlet />
+      <header className="bg-gray-900 text-white shadow-md">
+        <div className="max-w-6xl mx-auto py-10 px-4 md:px-0 flex items-center">
+          <h1 className="text-2xl uppercase font-semibold">
+            Products Management
+          </h1>
+        </div>
+      </header>
+      <main className="max-w-6xl mx-auto mt-10 px-4 md:px-0">
+        <section className="p-10 bg-white shadow-md ">
+            <Outlet />
+        </section>
+      </main>
     </>
   );
 }
