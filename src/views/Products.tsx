9d127d00001fb287d085a7566e468c8081999c80
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-23 15:22:52 -0600

    ✨ feat: enhance Products component with layout and Add Product link

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,7 +1,16 @@
-
+import { Link } from "react-router-dom";
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <div className="flex flex-col md:flex-row gap-4 justify-between items-center">
+      <h1 className="text-2xl font-medium text-gray-900">Products</h1>
+      <Link
+        to="/products/new"
+        className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
+      >
+        {" "}
+        Add Product
+      </Link>
+    </div>
+  );
 }
