47a91e0c69d57dc79d94b5336b4efe87772791b3
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-23 15:22:48 -0600

    ✨ feat: implement NewProduct component with form for product creation

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,6 +1,52 @@
+import { Link } from "react-router-dom";
 
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
-  )
+    <>
+      <div className="flex flex-col md:flex-row gap-4 justify-between items-center">
+        <h1 className="text-2xl font-medium text-gray-900">
+          Create New Product
+        </h1>
+        <Link
+          to="/"
+          className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
+        >
+          {" "}
+          Back to Products
+        </Link>
+      </div>
+
+      <form className="mt-10">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
+          value="Registrar Producto"
+        />
+      </form>
+    </>
+  );
 }
