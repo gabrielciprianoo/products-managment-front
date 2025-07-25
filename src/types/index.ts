b4d3b5c3e60086e83857542d3bd5ffdde9b49cc9
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-24 18:13:06 -0600

    ✨ feat: update ProductType and add DrafProductType for improved type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,10 @@
+import type { InferOutput } from "valibot"
+import type { DraftProductSchema } from "../schemas"
 
 export type ProductType = { 
     name: string,
     price: number,
     availability?: boolean
 }
+
+export type DrafProductType = InferOutput<typeof DraftProductSchema> 
