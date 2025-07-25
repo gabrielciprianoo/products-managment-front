f36a1763e05e65cd51ca7aa96b14c7b37ca3d8ab
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-24 19:23:07 -0600

    ✨ feat: add ToastContainer for toast notifications in main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { router } from './router'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { router } from "./router";
+import { RouterProvider } from "react-router-dom";
+import { Bounce, ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
+  </StrictMode>
+);
