3c0772780538f631dcb7b2c0f993dec0dee7024a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-26 09:04:33 -0600

    ✨ feat: enhance EditProduct component to include availability selection

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -5,12 +5,14 @@ import type { ProductType } from "../types";
 export default function EditProduct() {
   const product = useLoaderData() as ProductType;
   const error = useActionData() as string;
+  const availabilityOptions = [
+    { name: "Available", value: true },
+    { name: "Unavailable", value: false },
+  ];
   return (
     <>
       <div className="flex flex-col md:flex-row gap-4 justify-between items-center">
-        <h1 className="text-2xl font-medium text-gray-900">
-          Edit Product
-        </h1>
+        <h1 className="text-2xl font-medium text-gray-900">Edit Product</h1>
         <Link
           to="/"
           className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
@@ -20,7 +22,7 @@ export default function EditProduct() {
         </Link>
       </div>
 
-      {error && (<ErrorMessage>{error}</ErrorMessage>)}
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <Form className="mt-10" method="PUT">
         <div className="mb-4">
@@ -48,6 +50,23 @@ export default function EditProduct() {
             name="price"
             defaultValue={product.price}
           />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="availability">
+            Disponibilidad:
+          </label>
+          <select
+            id="availability"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map((option) => (
+              <option key={option.name} value={option.value.toString()}>
+                {option.name}
+              </option>
+            ))}
+          </select>
         </div>
         <input
           type="submit"
