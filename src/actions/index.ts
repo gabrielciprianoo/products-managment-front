428738aab86e2ef80a000e19992d267ce9bace7a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-26 10:14:30 -0600

    ♻️ refactor: import deleteProduct in index.ts and ensure deleteProductAction is properly defined

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,10 @@
 import { redirect, type ActionFunctionArgs } from "react-router-dom";
 import { parseFormData, validateProductForm } from "../utils";
 import type { DrafProductType, ProductType } from "../types";
-import { addProduct, updateProduct } from "../services";
+import { addProduct, deleteProduct, updateProduct } from "../services";
 import { notify } from "../utils/notify";
 
-export const addProductAction = async ({ request}: ActionFunctionArgs) => {
+export const addProductAction = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const rawData = parseFormData<ProductType>(formData);
 
@@ -22,15 +22,17 @@ export const addProductAction = async ({ request}: ActionFunctionArgs) => {
   return error;
 };
 
-export const updateProductAction = async ({ request, params}: ActionFunctionArgs
-) => {
+export const updateProductAction = async ({
+  request,
+  params,
+}: ActionFunctionArgs) => {
   const formData = await request.formData();
   const rawData = parseFormData<DrafProductType>(formData);
   const id = params.id;
   const data: DrafProductType = {
     name: rawData.name ?? "",
     price: parseFloat(String(rawData.price ?? "0")),
-    availability: String(rawData.availability) === "true" 
+    availability: String(rawData.availability) === "true",
   };
   const error = validateProductForm(data);
 
@@ -40,4 +42,15 @@ export const updateProductAction = async ({ request, params}: ActionFunctionArgs
     return redirect("/");
   }
   return error;
+};
+
+export const deleteProductAction = async ({ params }: ActionFunctionArgs) => {
+  const id = params.id;
+  if (!id) return redirect("/");
+  const result = await deleteProduct(Number(id));
+  if (!result) {
+    notify("error", "Error ocurred");
+  }
+  notify("success", "Product Deleted Successfully");
+  return redirect("/");
 };
