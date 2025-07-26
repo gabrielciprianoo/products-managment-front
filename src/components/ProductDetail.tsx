78d4626ec2be9c0b9440beb0fec96dfb2555fc02
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-26 10:04:38 -0600

    ♻️ refactor: update ProductDetail component layout and ensure DeleteProductButton is properly integrated

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,6 +1,7 @@
 import type { ProductType } from "../types";
 import { formatCurrency } from "../utils";
 import { useNavigate } from "react-router-dom";
+import DeleteProductButton from "./DeleteProductButton";
 
 type Props = {
   product: ProductType;
@@ -17,13 +18,15 @@ export default function ProductDetail({ product }: Props) {
       <td className="p-3 text-lg text-gray-800">{formatCurrency(price)}</td>
       <td className="p-3 text-lg text-gray-800">{isAvailable}</td>
       <td className="p-3 text-lg text-gray-800 ">
-        <div className="flex gap-2 items-center">
-          <button 
-          className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
-          onClick={ () => navigate(`/products/${id}/edit`)}
+        <div className="flex flex-col lg:flex-row gap-2 items-center">
+          <button
+            className="p-2 w-full bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
+            onClick={() => navigate(`/products/${id}/edit`)}
           >
             Edit
           </button>
+
+          <DeleteProductButton id={id}/>
         </div>
       </td>
     </tr>
