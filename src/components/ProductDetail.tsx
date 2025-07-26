0965d44afb0b1c12426a863da49ddde5d7446d2c
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-26 08:21:49 -0600

    🐛 fix: refactor ProductDetail component for improved readability and consistency

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,28 +1,31 @@
-import type { ProductType } from "../types"
-import { formatCurrency } from "../utils"
+import type { ProductType } from "../types";
+import { formatCurrency } from "../utils";
+import { useNavigate } from "react-router-dom";
 
-type Props = { 
-    product: ProductType
-}
+type Props = {
+  product: ProductType;
+};
 
-export default function ProductDetail( { product } : Props) {
-    
-    const { name, price, availability} = product
-    const isAvailable = availability ? 'Available' : 'Unavailable '
+export default function ProductDetail({ product }: Props) {
+  const { id, name, price, availability } = product;
+  const isAvailable = availability ? "Available" : "Unavailable ";
+  const navigate = useNavigate();
+
   return (
     <tr className="border-b ">
-        <td className="p-3 text-lg text-gray-800">
-            { name }
-        </td>
-        <td className="p-3 text-lg text-gray-800">
-            { formatCurrency(price) }
-        </td>
-        <td className="p-3 text-lg text-gray-800">
-            { isAvailable }
-        </td>
-        <td className="p-3 text-lg text-gray-800 ">
-            
-        </td>
-    </tr> 
-  )
+      <td className="p-3 text-lg text-gray-800">{name}</td>
+      <td className="p-3 text-lg text-gray-800">{formatCurrency(price)}</td>
+      <td className="p-3 text-lg text-gray-800">{isAvailable}</td>
+      <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center">
+          <button 
+          className="p-2 bg-blue-700 text-sm uppercase cursor-pointer text-white font-bold rounded-md hover:bg-blue-800 transition-colors"
+          onClick={ () => navigate(`/products/${id}/edit`)}
+          >
+            Edit
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
 }
