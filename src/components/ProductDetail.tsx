2614cbaf21cae47d77a73b45c8a0737bc66eb086
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-28 10:09:33 -0600

    ♻️ refactor: integrate fetcher for product availability button in ProductDetail component

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,6 +1,6 @@
 import type { ProductType } from "../types";
 import { formatCurrency } from "../utils";
-import { useNavigate } from "react-router-dom";
+import { useFetcher, useNavigate } from "react-router-dom";
 import DeleteProductButton from "./DeleteProductButton";
 
 type Props = {
@@ -11,12 +11,26 @@ export default function ProductDetail({ product }: Props) {
   const { id, name, price, availability } = product;
   const isAvailable = availability ? "Available" : "Unavailable ";
   const navigate = useNavigate();
+  const fetcher = useFetcher();
 
   return (
     <tr className="border-b ">
       <td className="p-3 text-lg text-gray-800">{name}</td>
       <td className="p-3 text-lg text-gray-800">{formatCurrency(price)}</td>
-      <td className="p-3 text-lg text-gray-800">{isAvailable}</td>
+      <td className="p-3 text-lg text-gray-800">
+        <fetcher.Form className="w-1" method="POST">
+          <button
+            type="submit"
+            className={` text-sm uppercase border-2 p-1 font-bold border-black cursor-pointer hover:bg-gray-300 transition- ${
+              availability ? "text-black" : "text-red-800"
+            }`}
+            value={id}
+            name="id"
+          >
+            {isAvailable}
+          </button>
+        </fetcher.Form>
+      </td>
       <td className="p-3 text-lg text-gray-800 ">
         <div className="flex flex-col lg:flex-row gap-2 items-center">
           <button
@@ -26,7 +40,7 @@ export default function ProductDetail({ product }: Props) {
             Edit
           </button>
 
-          <DeleteProductButton id={id}/>
+          <DeleteProductButton id={id} />
         </div>
       </td>
     </tr>
