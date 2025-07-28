3286b0b671d9359860f6c6599943787a0fcb537d
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-28 10:09:14 -0600

    ♻️ refactor: add updateAvailability action to Products route in router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from './layouts'
 import { EditProduct, NewProduct, Products } from './views'
 import { loader as productLoader } from './loaders/productsLoader'
-import { addProductAction, deleteProductAction, updateProductAction } from './actions'
+import { addProductAction, deleteProductAction, updateAvailability, updateProductAction } from './actions'
 import { loader as editProductLoader } from './loaders/editProductLoader'
 
 export const router = createBrowserRouter([
@@ -13,7 +13,8 @@ export const router = createBrowserRouter([
             {
                 element: <Products/>,
                 index: true,
-                loader: productLoader
+                loader: productLoader,
+                action: updateAvailability
             },
             {
                 element: <NewProduct/>,
